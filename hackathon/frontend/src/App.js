d06b10e149a94146f1abef4878b919927da9b1e2
Fábio Lourenço <faboyds@gmail.com>
2020-03-08 13:27:47 +0000

    Base project for Hackathon website

---
+++ hackathon/frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar/>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <SignIn/>
+            <SignUp/>
+        </div>
+    );
+}
+
+export default App;
