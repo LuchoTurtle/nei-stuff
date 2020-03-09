3686fbc1a08c92b9cd77bd6bea904931bd0df54c
1150625_LuisArteiro <1150625@isep.ipp.pt>
2020-03-09 14:38:38 +0000

    [Hackathon20] Adding stuff to header

--- hackathon/frontend/src/components/PageHeader/PageHeader.jsx
+++ hackathon/frontend/src/components/PageHeader/PageHeader.jsx
@@ -19,25 +19,33 @@ import React from "react";
 
 // reactstrap components
 import { Container } from "reactstrap";
+import Typing from 'react-typing-animation';
+
+import "../../styles/glitch.scss"
 
 class PageHeader extends React.Component {
   render() {
     return (
       <div className="page-header header-filter">
-        <div className="squares square1" />
-        <div className="squares square2" />
-        <div className="squares square3" />
-        <div className="squares square4" />
-        <div className="squares square5" />
-        <div className="squares square6" />
-        <div className="squares square7" />
+          {
+              /*
+              <div className="squares square1" />
+              <div className="squares square2" />
+              <div className="squares square3" />
+              <div className="squares square4" />
+              <div className="squares square5" />
+              <div className="squares square6" />
+              <div className="squares square7" />
+               */
+          }
         <Container>
           <div className="content-center brand">
-            <h1 className="h1-seo">BLK• React</h1>
-            <h3 className="d-none d-sm-block">
-              A beautiful Design System for Bootstrap 4 (reactstrap) and React.
-              It's Free and Open Source.
-            </h3>
+            <h1 className="glitch" data-text="Git Overflow">Git Overflow</h1>
+            <Typing speed={5}>
+              <h3 style={{textTransform: "lowercase"}}>
+                A maior hackathon do ISEP chegou para ficar.
+              </h3>
+            </Typing>
           </div>
         </Container>
       </div>
