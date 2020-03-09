3330236003f716ed4aebd2077a40727584b0188d
1150625_LuisArteiro <1150625@isep.ipp.pt>
2020-03-09 15:21:11 +0000

    [Hackathon] Header done.

--- hackathon/frontend/src/components/PageHeader/PageHeader.jsx
+++ hackathon/frontend/src/components/PageHeader/PageHeader.jsx
@@ -18,39 +18,60 @@
 import React from "react";
 
 // reactstrap components
-import { Container } from "reactstrap";
+import {Container} from "reactstrap";
 import Typing from 'react-typing-animation';
+import Particles from "react-particles-js";
 
 import "../../styles/glitch.scss"
 
 class PageHeader extends React.Component {
-  render() {
-    return (
-      <div className="page-header header-filter">
-          {
-              /*
-              <div className="squares square1" />
-              <div className="squares square2" />
-              <div className="squares square3" />
-              <div className="squares square4" />
-              <div className="squares square5" />
-              <div className="squares square6" />
-              <div className="squares square7" />
-               */
-          }
-        <Container>
-          <div className="content-center brand">
-            <h1 className="glitch" data-text="Git Overflow">Git Overflow</h1>
-            <Typing speed={5}>
-              <h3 style={{textTransform: "lowercase"}}>
-                A maior hackathon do ISEP chegou para ficar.
-              </h3>
-            </Typing>
-          </div>
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="page-header header-filter">
+                {
+                    /*
+                    <div className="squares square1" />
+                    <div className="squares square2" />
+                    <div className="squares square3" />
+                    <div className="squares square4" />
+                    <div className="squares square5" />
+                    <div className="squares square6" />
+                    <div className="squares square7" />
+                     */
+                }
+                <Container>
+                    <Particles
+                        height={"100vh"}
+                        params={{
+                            "particles": {
+                                "number": {
+                                    "value": 50
+                                },
+                                "size": {
+                                    "value": 3
+                                }
+                            },
+                            "interactivity": {
+                                "events": {
+                                    "onhover": {
+                                        "enable": true,
+                                        "mode": "repulse"
+                                    }
+                                }
+                            }
+                        }}/>
+                    <div className="content-center brand">
+                        <h1 className="glitch" data-text="Git Overflow">Git Overflow</h1>
+                        <Typing speed={5}>
+                            <h3 style={{textTransform: "lowercase"}}>
+                                A maior hackathon do ISEP chegou para ficar.
+                            </h3>
+                        </Typing>
+                    </div>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default PageHeader;
