f17711225711887a5f97ae33acbe259aae7f2b5b
Luis Arteiro <lucho14garfield@gmail.com>
2020-03-11 18:04:08 +0000

    [Hackathon] Privacy policy

--- hackathon/frontend/src/components/Sections/SectionRegister.js
+++ hackathon/frontend/src/components/Sections/SectionRegister.js
@@ -103,7 +103,7 @@ class SectionRegister extends React.Component {
         this.setState({passwordSame: this.state.password === this.state.repeatPassword && this.state.password !== ""})
     }
 
-    toggleModalLarge(){
+    toggleModalLarge() {
         this.setState({
             modalLarge: !this.state.modalLarge
         });
@@ -117,12 +117,12 @@ class SectionRegister extends React.Component {
 
         this.setState({error: ""});
 
-        if(email === "" ||  username === "" || !passwordSame) {
+        if (email === "" || username === "" || !passwordSame) {
             this.setState({emailEmpty: email === "", usernameEmpty: username === "", passwordSame: false});
             return
         }
 
-        if(!termsAgreed) {
+        if (!termsAgreed) {
             this.setState({error: "Tens de aceitar os termos e condições para te registares."})
             return
         }
@@ -135,7 +135,7 @@ class SectionRegister extends React.Component {
             <div id="register-section">
                 <div className="wrapper">
                     <div className="page-header">
-                        <div className="page-header-image" />
+                        <div className="page-header-image"/>
                         <div className="content">
                             <Container style={{paddingTop: "0px"}}>
                                 <Row>
@@ -143,12 +143,12 @@ class SectionRegister extends React.Component {
                                         <div
                                             className="square square-7"
                                             id="square7"
-                                            style={{ transform: this.state.squares7and8 }}
+                                            style={{transform: this.state.squares7and8}}
                                         />
                                         <div
                                             className="square square-8"
                                             id="square8"
-                                            style={{ transform: this.state.squares7and8 }}
+                                            style={{transform: this.state.squares7and8}}
                                         />
                                         <Card className="card-register">
                                             <CardHeader>
@@ -162,45 +162,51 @@ class SectionRegister extends React.Component {
                                                 <Form className="form">
                                                     <InputGroup
                                                         className={classnames({
-                                                            "input-group-focus": this.state.fullNameFocus
-                                                        },
+                                                                "input-group-focus": this.state.fullNameFocus
+                                                            },
                                                             {"has-danger": this.state.usernameEmpty === true}
-                                                            )}
+                                                        )}
                                                     >
                                                         <InputGroupAddon addonType="prepend">
                                                             <InputGroupText>
-                                                                <i className="tim-icons icon-single-02" />
+                                                                <i className="tim-icons icon-single-02"/>
                                                             </InputGroupText>
                                                         </InputGroupAddon>
                                                         <Input
                                                             placeholder="Username"
                                                             type="text"
                                                             onFocus={e =>
-                                                                this.setState({ fullNameFocus: true })
+                                                                this.setState({fullNameFocus: true})
                                                             }
                                                             onBlur={e =>
-                                                                this.setState({ fullNameFocus: false })
+                                                                this.setState({fullNameFocus: false})
                                                             }
-                                                            onChange={e => this.setState({username: e.target.value, usernameEmpty: null})}
+                                                            onChange={e => this.setState({
+                                                                username: e.target.value,
+                                                                usernameEmpty: null
+                                                            })}
                                                         />
                                                     </InputGroup>
                                                     <InputGroup
                                                         className={classnames({
-                                                            "input-group-focus": this.state.emailFocus
-                                                        },
+                                                                "input-group-focus": this.state.emailFocus
+                                                            },
                                                             {"has-danger": this.state.emailEmpty === true})}
                                                     >
                                                         <InputGroupAddon addonType="prepend">
                                                             <InputGroupText>
-                                                                <i className="tim-icons icon-email-85" />
+                                                                <i className="tim-icons icon-email-85"/>
                                                             </InputGroupText>
                                                         </InputGroupAddon>
                                                         <Input
                                                             placeholder="Email"
                                                             type="text"
-                                                            onFocus={e => this.setState({ emailFocus: true })}
-                                                            onBlur={e => this.setState({ emailFocus: false })}
-                                                            onChange={e => this.setState({email: e.target.value, emailEmpty: null})}
+                                                            onFocus={e => this.setState({emailFocus: true})}
+                                                            onBlur={e => this.setState({emailFocus: false})}
+                                                            onChange={e => this.setState({
+                                                                email: e.target.value,
+                                                                emailEmpty: null
+                                                            })}
                                                         />
                                                     </InputGroup>
                                                     <InputGroup
@@ -211,17 +217,17 @@ class SectionRegister extends React.Component {
                                                     >
                                                         <InputGroupAddon addonType="prepend">
                                                             <InputGroupText>
-                                                                <i className="tim-icons icon-lock-circle" />
+                                                                <i className="tim-icons icon-lock-circle"/>
                                                             </InputGroupText>
                                                         </InputGroupAddon>
                                                         <Input
                                                             placeholder="Password"
                                                             type="password"
                                                             onFocus={e =>
-                                                                this.setState({ passwordFocus: true })
+                                                                this.setState({passwordFocus: true})
                                                             }
                                                             onBlur={e =>
-                                                                this.setState({ passwordFocus: false })
+                                                                this.setState({passwordFocus: false})
                                                             }
                                                             onChange={async e => {
                                                                 await this.setState({password: e.target.value});
@@ -232,23 +238,23 @@ class SectionRegister extends React.Component {
                                                     </InputGroup>
                                                     <InputGroup
                                                         className={classnames({
-                                                            "input-group-focus": this.state.repeatPasswordFocus
-                                                        },
+                                                                "input-group-focus": this.state.repeatPasswordFocus
+                                                            },
                                                             {"has-danger": this.state.passwordSame === false})}
                                                     >
                                                         <InputGroupAddon addonType="prepend">
                                                             <InputGroupText>
-                                                                <i className="tim-icons icon-lock-circle" />
+                                                                <i className="tim-icons icon-lock-circle"/>
                                                             </InputGroupText>
                                                         </InputGroupAddon>
                                                         <Input
                                                             placeholder="Repetir password"
                                                             type="password"
                                                             onFocus={e =>
-                                                                this.setState({ repeatPasswordFocus: true })
+                                                                this.setState({repeatPasswordFocus: true})
                                                             }
                                                             onBlur={e =>
-                                                                this.setState({ repeatPasswordFocus: false })
+                                                                this.setState({repeatPasswordFocus: false})
                                                             }
                                                             onChange={async e => {
                                                                 await this.setState({repeatPassword: e.target.value});
@@ -260,9 +266,9 @@ class SectionRegister extends React.Component {
                                                     <FormGroup check className="text-left">
                                                         <Label check>
                                                             <Input type="checkbox"
-                                                                    onChange={e => this.setState({agreedWithTerms: e.target.checked})}
+                                                                   onChange={e => this.setState({agreedWithTerms: e.target.checked})}
                                                             />
-                                                            <span className="form-check-sign" />Eu concordo com{" "}
+                                                            <span className="form-check-sign"/>Eu concordo com{" "}
                                                             <a
                                                                 href="#pablo"
                                                                 onClick={e => {
@@ -280,38 +286,39 @@ class SectionRegister extends React.Component {
                                                 </Form>
                                             </CardBody>
                                             <CardFooter style={{textAlign: "center"}}>
-                                                <Button className="btn-round" color="primary" size="lg" onClick={this.submit}>
+                                                <Button className="btn-round" color="primary" size="lg"
+                                                        onClick={this.submit}>
                                                     Registar
                                                 </Button>
                                             </CardFooter>
                                         </Card>
                                     </Col>
                                 </Row>
-                                <div className="register-bg" />
+                                <div className="register-bg"/>
                                 <div
                                     className="square square-1"
                                     id="square1"
-                                    style={{ transform: this.state.squares1to6 }}
+                                    style={{transform: this.state.squares1to6}}
                                 />
                                 <div
                                     className="square square-2"
                                     id="square2"
-                                    style={{ transform: this.state.squares1to6 }}
+                                    style={{transform: this.state.squares1to6}}
                                 />
                                 <div
                                     className="square square-4"
                                     id="square4"
-                                    style={{ transform: this.state.squares1to6 }}
+                                    style={{transform: this.state.squares1to6}}
                                 />
                                 <div
                                     className="square square-5"
                                     id="square5"
-                                    style={{ transform: this.state.squares1to6 }}
+                                    style={{transform: this.state.squares1to6}}
                                 />
                                 <div
                                     className="square square-6"
                                     id="square6"
-                                    style={{ transform: this.state.squares1to6 }}
+                                    style={{transform: this.state.squares1to6}}
                                 />
                             </Container>
                         </div>
@@ -320,10 +327,17 @@ class SectionRegister extends React.Component {
 
                 <Modal isOpen={this.state.modalLarge} toggle={this.toggleModalLarge} size="lg">
                     <ModalHeader className="justify-content-center" toggle={this.toggleModalLarge}>
-                        Large modal
+                        Política de privacidade
                     </ModalHeader>
                     <ModalBody>
-                        ...
+                        <p>Todas as suas informações pessoais recolhidas, serão usadas para o ajudar a tornar a sua
+                            visita no nosso site o mais produtiva e agradável possível.</p>
+                        <p>A garantia da
+                        confidencialidade dos dados pessoais dos utilizadores do nosso site é importante para o NEI-ISEP. Dados sensíveis não irão ser partilhados
+                        e irão ser mantidos com segurança.</p>
+                        <p>Todas as informações pessoais relativas aos participantes do Git Overflow Hackathon serão tratadas em concordância com a Lei da Proteção de Dados
+                        Pessoais de 26 de outubro de 1998 (Lei n.º 67/98).</p>
+                        <p>A informação pessoal recolhida pode inclui o seu e-mail apenas para notificações do progresso do evento.</p>
                     </ModalBody>
                 </Modal>
             </div>
